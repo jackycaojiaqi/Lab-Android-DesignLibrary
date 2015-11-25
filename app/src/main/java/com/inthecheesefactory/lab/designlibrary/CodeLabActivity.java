a13eb0450ab6f6b1dd0c7a30c2d1989e5a7b5c6d
当小新遇上小丸子 <353938360@qq.com>
2015-11-25 17:03:04 +0800

    compile 'com.android.support:design:23.1.0'  试用

--- app/src/main/java/com/inthecheesefactory/lab/designlibrary/CodeLabActivity.java
+++ app/src/main/java/com/inthecheesefactory/lab/designlibrary/CodeLabActivity.java
@@ -1,23 +1,30 @@
 package com.inthecheesefactory.lab.designlibrary;
 
 import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class CodeLabActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle drawerToggle;
-
-    @Override
+    CoordinatorLayout rootLayout;
+    Toolbar toolbar;
+    TabLayout tabLayout;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code_lab);
-        
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         initInstances();
     }
 
@@ -25,9 +32,17 @@ public class CodeLabActivity extends AppCompatActivity {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         drawerToggle = new ActionBarDrawerToggle(CodeLabActivity.this, drawerLayout, R.string.hello_world, R.string.hello_world);
         drawerLayout.setDrawerListener(drawerToggle);
-
+        rootLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Snackbar.make(rootLayout, "Hello. I am Snackbar!", Snackbar.LENGTH_LONG)
+                .setAction("Do", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                })
+                .show();
     }
 
     @Override
